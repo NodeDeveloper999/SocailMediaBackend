f46f9308e63bf1a1842c990a21e19156bd56da61
NodeDeveloper999 <raheem@biit.edu.pk>
2025-05-13 21:30:04 +0500

    cors changes

--- server.js
+++ server.js
@@ -18,9 +18,12 @@ app.use(express.json());
 app.use(cors({
   origin: 'https://social-app-8ox3.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-
+allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 
 }));
+app.options('*', cors()); // handle preflight for all routes
+
 app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', authRoutes);
