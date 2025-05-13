1c0ea4a8e9aeccc9f8309d56ebb61aa74cd45f26
NodeDeveloper999 <raheem@biit.edu.pk>
2025-05-13 21:34:57 +0500

    hello

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors({
   origin: 'https://social-app-8ox3.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-allowedHeaders: ['Content-Type', 'Authorization'],
+
   credentials: true,
 
 }));
